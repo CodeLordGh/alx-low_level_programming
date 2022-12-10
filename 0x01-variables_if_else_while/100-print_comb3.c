2754730b8a234662cf6cc855af882220674d471c
CodeLordGh <babohkossi228@icloud.com>
2022-12-10 06:56:15 -0800

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,16 @@ int main(void)
 	int singles = '0';
 	int doubles = '0';
 
-	for (doubles = '0'; doubles <= 9; doubles++)
+	for (doubles = 0; doubles <= 9; doubles++)
 	{
-		for (singles = '0'; singles <= '9'; singles++)
+		for (singles = 0; singles <= '9'; singles++)
 		{
-			if (!(doubles > singles) || (singles == doubles))
+			if (!((doubles > singles) || (singles == doubles)))
 			{
 				putchar(doubles + '0');
 				putchar(singles + '0');
 
-				if (!(singles == '9') && (doubles == '8'))
+				if (!((singles == 9) && (doubles == 8)))
 				{
 					putchar(',');
 					putchar(' ');
