4613533a243439127ba8b003cb8b8b699bdd3313
CodeLordGh <babohkossi228@icloud.com>
2022-12-10 06:49:40 -0800

    combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints all possible different combinations of two digits.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int singles = '0';
+	int doubles = '0';
+
+	for (doubles = '0'; doubles <= 9; doubles++)
+	{
+		for (singles = '0'; singles <= '9'; singles++)
+		{
+			if (!(doubles > singles) || (singles == doubles))
+			{
+				putchar(doubles + '0');
+				putchar(singles + '0');
+
+				if (!(singles == '9') && (doubles == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
