1970954230878c51791585778d062df9ac4364cb
CodeLordGh <www.babohkossi228@icloud.com>
2022-12-19 16:05:41 +0000

    Don't swap horses in crossing a stream mandatory

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int k;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
